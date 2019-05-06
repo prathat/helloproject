5656ba9113b9952739d3d7f3ba4a0b253519b066
prathat <yespratheesh@gmail.com>
2019-05-06 19:03:47 +0530

    Added hello project files

---
+++ helloproject/helloapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+import re
+from datetime import datetime
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, World! This is an http response from django`s webserver")
+def page1(request):
+    return HttpResponse("Hello, World! This is PAGE1")
+def timepage(request,name):
+    return render(request,'helloapp/timepage.html',{"name": name, "date": datetime.now()})
+    
+    
